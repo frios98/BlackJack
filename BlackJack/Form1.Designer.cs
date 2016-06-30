dd13299184130db5b13e20c83f6fcf5c41720bf3
Filiberto Rios <rios.filiberto.98@gmail.com>
2016-06-30 10:41:15 -0500

    Added AI mechanics

--- BlackJack/Form1.Designer.cs
+++ BlackJack/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.lblPlayerCards = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.lblDealerHand = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
@@ -47,9 +49,10 @@
             // lblPlayerCards
             // 
             this.lblPlayerCards.AutoSize = true;
-            this.lblPlayerCards.Location = new System.Drawing.Point(269, 344);
+            this.lblPlayerCards.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerCards.Location = new System.Drawing.Point(216, 342);
             this.lblPlayerCards.Name = "lblPlayerCards";
-            this.lblPlayerCards.Size = new System.Drawing.Size(10, 13);
+            this.lblPlayerCards.Size = new System.Drawing.Size(16, 24);
             this.lblPlayerCards.TabIndex = 2;
             this.lblPlayerCards.Text = "-";
             // 
@@ -66,17 +69,38 @@
             // lblDealerHand
             // 
             this.lblDealerHand.AutoSize = true;
-            this.lblDealerHand.Location = new System.Drawing.Point(272, 56);
+            this.lblDealerHand.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerHand.Location = new System.Drawing.Point(219, 54);
             this.lblDealerHand.Name = "lblDealerHand";
-            this.lblDealerHand.Size = new System.Drawing.Size(10, 13);
+            this.lblDealerHand.Size = new System.Drawing.Size(16, 24);
             this.lblDealerHand.TabIndex = 4;
             this.lblDealerHand.Text = "-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(292, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Dealer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(328, 369);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Player";
             // 
             // JackBlack
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(673, 472);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblDealerHand);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblPlayerCards);
@@ -93,6 +117,8 @@
         private System.Windows.Forms.Label lblPlayerCards;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lblDealerHand;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- BlackJack/Form1.cs
+++ BlackJack/Form1.cs
@@ -20,6 +20,7 @@ namespace BlackJack
             BlackJack.Properties.Resources.card9, BlackJack.Properties.Resources.card10, BlackJack.Properties.Resources.card11,
             BlackJack.Properties.Resources.card12 };
         List<int> playerHand = new List<int>();
+        List<int> aiHand = new List<int>();
 
         public JackBlack()
         {
@@ -33,6 +34,11 @@ namespace BlackJack
         private void showHand()
         {
             lblPlayerCards.Text = string.Join(", ", playerHand.ToArray());
+        }
+
+        private void showDealerHand()
+        {
+            lblDealerHand.Text = string.Join(", ", aiHand.ToArray());
         }
 
         private int randomCard()
@@ -55,12 +61,12 @@ namespace BlackJack
             if (total == 21)
             {
                 //end game, money goes to player and resets game
-                resetGame(true);
+                resetGame();
             }
             else if (total > 21)
             {
                 //bust, vice versa
-                resetGame(false);
+                resetGame();
             }
         }
 
@@ -68,23 +74,56 @@ namespace BlackJack
         {
             playerHand.Add(randomCard());
         }
-
+        //fold method
         private void button1_Click(object sender, EventArgs e)
         {
+            button2.Enabled = false;
+            button1.Enabled = false;
             addHand();
+            aiTurn();
         }
 
-        private void resetGame(bool playerWon)
+        private void resetGame()
         {
-            if (playerWon)
-                MessageBox.Show("You Won. Feel great about yourself.");
-
-            else
+            int p = playerHand.Sum();
+            int ai = aiHand.Sum();
+            if (p > 21)
+            {
                 MessageBox.Show("Sorry. No winning for you");
+            }
+            else if (p == 21 || p > ai )
+            {
+                MessageBox.Show("You Won. Feel great about yourself.");
+            }
+            else if (ai > p)
+            {
+                MessageBox.Show("Sorry. No winning for you");
+            }
+            else if (p == ai)
+            {
+                MessageBox.Show("It's a Draw!");
+            }
+                
             playerHand.Clear();
+            aiHand.Clear();
+            showDealerHand();
             dealCard();
             dealCard();
             showHand();
+            button2.Enabled = true;
+            button1.Enabled = true;
+        }
+
+        private void aiTurn()
+        {
+            aiHand.Add(randomCard());
+            aiHand.Add(randomCard());
+            while (aiHand.Sum() <= 17)
+            {
+                aiHand.Add(randomCard());
+            }
+            showDealerHand();
+            resetGame();
         }
     }
 }
